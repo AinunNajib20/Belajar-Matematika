b4805bc4c6b8e04b19bd0258e4bde9f646faa96a
Ainun Najib <ainun.najib20@hotmail.com>
2023-12-23 13:07:51 +0700

    first commit

---
+++ Assets/script/BtnExit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BtnExit : MonoBehaviour {
+
+	public GameObject panelExit;
+
+	public void ExitControl ()
+	{
+		if (Time.timeScale ==1){
+			panelExit.SetActive (true);
+			Time.timeScale = 0;
+		} else {
+			Time.timeScale = 1;
+			panelExit.SetActive (false);
+		}
+	}
+
+	public void scene(string scene){
+		Application.LoadLevel (scene);
+	}
+
+	public void quit (){
+		Application.Quit ();
+	}
+}
